f7feb6c15ede9fbbc04a0d33bd96d7b261971c92
qcy <60570697+qicaiy@users.noreply.github.com>
2025-04-22 08:17:49 +0800

    修改最低版本为34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.malla.testapplist"
-        minSdk = 35
+        minSdk = 34
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
